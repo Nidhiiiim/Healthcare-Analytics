23888b38e86bc0cf04db19f86a2d3895a41497d8
Nidhi Mishra <113460614+Nidhiiiim@users.noreply.github.com>
2024-06-09 16:55:16 -0400

    diagnosis Analytics

---
+++ Diagnosis_Treatment_Analysis.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+# initializing spark session
+spark = SparkSession.builder.appName("Healthcare Data Analysis").getOrCreate()
+
+# Loading data
+healthcare_data = spark.read.csv("/Users/nidhimishra/Downloads/healthcare_dataset.csv", header=True, inferSchema=True)
+
+#Common Diagnosis
+common_diagnosis = healthcare_data.groupby("Medical Condition").count().orderBy(col("count").desc())
+common_diagnosis.show()
+
+#Treatment Diagnosis
+TreatmentEffectiveness = healthcare_data.groupby("Medication", "Test Results").count()
+TreatmentEffectiveness.show()
